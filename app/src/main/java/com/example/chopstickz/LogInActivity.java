e5cf64f3354fd1fd94d25ff73fec4d2bf981dde0
EZEAGU DAVID <cds@EZEAGUs-MacBook-Pro.local>
2019-11-25 19:21:05 +0100

    done login feature

--- app/src/main/java/com/example/chopstickz/LogInActivity.java
+++ app/src/main/java/com/example/chopstickz/LogInActivity.java
@@ -1,38 +1,153 @@
 package com.example.chopstickz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+
+    private static final String TAG = "LogInActivity";
+    private static final int RC_LOG_IN = 9001;
+
+    private Button mLogInButton;
+
+    private GoogleApiClient mGoogleApiClient;
+
+
+    private FirebaseAuth mFirebaseAuth;
+
     EditText username, pwd;
     Button btnLogIn;
     TextView tvForgotThePassword;
     TextView tvRegister;
-    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        mFirebaseAuth= FirebaseAuth.getInstance();
-        username = findViewById(R.id.username);
-        pwd = findViewById(R.id.pwd);
-        btnLogIn = findViewById(R.id.buttonLogin);
-        btnLogIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-            }
-        });
+        btnLogIn = findViewById(R.id.buttonLogin);
         tvForgotThePassword = findViewById(R.id.ftp);
         tvRegister = findViewById(R.id.reg);
+        btnLogIn.setOnClickListener(this);
+
+
+//                String email = username.getText().toString();
+//                String password = pwd.getText().toString();
+//                if(email.isEmpty()) {
+//                    username.setError("Please enter your email");
+//                    username.requestFocus();
+//                }
+//                else if(password.isEmpty()) {
+//                    pwd.setError("Please enter your password");
+//                    pwd.requestFocus();
+//                }
+
+// Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+
+    private void logIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+       startActivityForResult(signInIntent, RC_LOG_IN);
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_LOG_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign-In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign-In failed
+                Log.e(TAG, "Google Sign-In failed.");
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGooogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            startActivity(new Intent(LogInActivity.this, FoodSelectionActivity.class));
+                            finish();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        logIn();
     }
 }
+
+
+
+
+
--- app/src/main/java/com/example/chopstickz/MainActivity.java
+++ app/src/main/java/com/example/chopstickz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chopstickz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,11 +13,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 4000;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
 
     @Override
@@ -24,35 +29,86 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boolean isLoggedIn = false;
 
-        if (isLoggedIn) {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent homeIntent = new Intent(MainActivity.this, FoodSelectionActivity.class);
-                    startActivity(homeIntent);
-                    finish();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
-                }
-            }, SPLASH_TIME_OUT);
+
+                            if (mFirebaseUser == null) {
+                                // Not signed in, launch the Sign In activity
+
+
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        startActivity(new Intent(MainActivity.this, LogInActivity.class));
+                                        finish();
+                                    }
+
+                            },SPLASH_TIME_OUT);
+            return;
         } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(MainActivity.this, LogInActivity.class);
-                    startActivity(intent);
-                    finish();
-
-                }
-            }, SPLASH_TIME_OUT);
+            Intent homeIntent = new Intent(MainActivity.this, FoodSelectionActivity.class);
+                           startActivity(homeIntent);
+                           finish();
+            }
         }
 
+//        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+//                if (mFirebaseUser == null) {
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+//                            startActivity(intent);
+//                            finish();
+//
+//                        }
+//                    }, SPLASH_TIME_OUT);
+//
+//                } else {
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            Intent homeIntent = new Intent(MainActivity.this, FoodSelectionActivity.class);
+//                            startActivity(homeIntent);
+//                            finish();
+//
+//                        }
+//                    }, SPLASH_TIME_OUT);
+//                }
+//
+//            }
+//        };
+//
+//        boolean isLoggedIn = false;
+//
+//        if (isLoggedIn) {
+//
+//        } else {
+//
+//        }
 
+
+
+
+
+    private void storeLogInActivity(boolean userloggedin) {
+        SharedPreferences mSharedPrefences = getSharedPreferences("chopstickz", MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mSharedPrefences.edit();
+        mEditor.putBoolean("userloggedin", userloggedin);
+        mEditor.apply();
     }
 
 
-    private void storeLogInActivity{
-        SharedPreferences mSharedPrefences = getSharedPreferences(LogInActivity)
+    private boolean getBoolean() {
+        SharedPreferences mSharedPrefernces = getSharedPreferences("chopstickz", MODE_PRIVATE);
+        boolean selectedBoolean = mSharedPrefernces.getBoolean("userloggedin", false);
+        return selectedBoolean;
     }
+
+
 }
